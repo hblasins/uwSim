cc07feafd20dafb62e3f2e73e1e6085be82e1447
Henryk Blasinski <hblasins@stanford.edu>
2017-05-30 16:00:24 -0700

    Added a function to read camera images.

---
+++ Code/Utilities/readCameraImage.m
@@ -0,0 +1,36 @@
+function [ sensor,  cp, img, meta] = readCameraImage( path )
+
+[directory, file] = fileparts(path);
+
+% Read raw sensor image
+cmd = sprintf('dcraw -v -r 1 1 1 1 -H 0 -o 0 -d -j -4 %s',path);
+system(cmd);
+fName = fullfile(directory,sprintf('%s.pgm',file));
+img = double(imread(fName));
+img = img/max(img(:));
+delete(fName);
+
+% Read shared metadata
+fName = fullfile(directory,'params.xml');
+data1 = xml2struct(fName);
+
+% Read image specific metadata
+fName = fullfile(directory,sprintf('%s.xml',file));
+data2 = xml2struct(fName);
+
+meta = data1.parameters;
+fieldNames = fieldnames(data2.parameters);
+
+for i=1:size(fieldNames)
+    meta.(fieldNames{i}) = data2.parameters.(fieldNames{i});
+end
+
+% Create an ISET image sensor
+sensor = sensorCreate('bayer (gbrg)');
+sensor = sensorSet(sensor,'size',size(img));
+sensor = sensorSet(sensor,'volts',img);
+
+cp = eval(data2.parameters.macbethCorners.RAW.Text);
+
+end
+
