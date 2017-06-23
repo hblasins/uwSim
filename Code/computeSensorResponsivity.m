a5e87fcbb869ce6a861283994f88281c940cabe6
Henryk Blasinski <hblasins@stanford.edu>
2017-06-23 10:22:26 -0700

    Removed the dependency on DNG converter and CVX.

--- Code/computeSensorResponsivity.m
+++ Code/computeSensorResponsivity.m
@@ -35,39 +35,35 @@ rect = [2731 1707 76 64];
 
 % First convert to uncompressed DNG using Adobe DNG converter
 % (This may take a while).
-cmd = sprintf('''/Applications/Adobe DNG Converter.app/Contents/MacOS/Adobe DNG Converter'' -l -u %s/*.CR2',dataDir);
-system(cmd)
 
-files = dir(fullfile(dataDir,'*.dng'));
-nFiles = length(files);
+fNames = dir(fullfile(dataDir,'*.CR2'));
+nFiles = length(fNames);
 
 ISO = zeros(nFiles,1);
 shutter = zeros(nFiles,1);
 aperture = zeros(nFiles,1);
 data = zeros(nFiles,3);
 
-warning off MATLAB:tifflib:TIFFReadDirectory:libraryWarning
 for i=1:nFiles
    
-    fName = sprintf(fullfile(dataDir,files(i).name));
+    filePath = fullfile(dataDir,fNames(i).name);
+    cmd = sprintf('dcraw -v -r 1 1 1 1 -H 0 -o 0 -j -4 -q 0 %s',filePath);
+    system(cmd);
     
-    exif = imfinfo(fName);
+    [~, fileName] = fileparts(fNames(i).name);
+    fName = fullfile(dataDir,sprintf('%s.ppm',fileName));
+    img = double(imread(fName));
+        
+    exif = imfinfo(filePath);
     
     % We read in the shutter, ISO and the aperture, but
     % we assume these values are constant for all images and hence
     % ignore their impact on image intensities.
-    shutter(i) = exif.DigitalCamera.ExposureTime;
-    ISO(i) = exif.DigitalCamera.ISOSpeedRatings;
-    aperture(i) = exif.DigitalCamera.FNumber;
-    
-    t = Tiff(fName,'r');
-    offsets = getTag(t,'SubIFD');
-    setSubDirectory(t,offsets(1));
-    cfa = read(t);
-    
-    close(t);
-    
-    roi = cfa(rect(2):rect(2)+rect(4), rect(1):rect(1)+rect(3),:);
+    shutter(i) = exif(1).DigitalCamera.ExposureTime;
+    ISO(i) = exif(1).DigitalCamera.ISOSpeedRatings;
+    aperture(i) = exif(1).DigitalCamera.FNumber;
+        
+    roi = img(rect(2):rect(2)+rect(4), rect(1):rect(1)+rect(3),:);
     roi = reshape(roi,[(rect(3)+1)*(rect(4)+1), 3]);
     
     data(i,:) = mean(roi);
@@ -76,7 +72,8 @@ end
 
 % Remove .dng files
 for i=1:nFiles
-    delete(fullfile(dataDir,files(i).name));
+    [~, fileName] = fileparts(fNames(i).name);
+    delete(fullfile(dataDir,sprintf('%s.ppm',fileName)));
 end
 
 %% Compute the responsivity curves
@@ -100,12 +97,31 @@ R = R + [zeros(nWaves-1,1) diag(-1*ones(nWaves-1,1))];
 responsivity = zeros(nWaves,3);
 for i=1:3
 
+    % Now we solve a constrained quadratic program to find the
+    % responsivities. 
+    %
+    % Below is the CVX problem formulation
+    %{
     cvx_begin
         variable resp(nWaves,1)
         minimize norm(input*resp - data(:,i),2) + lambda*norm(R*resp,2)
         subject to 
             resp >= 0
     cvx_end
+    %}
+    
+    % Or Matlab optimization toolbox formulation
+    A = [input; sqrt(lambda)*R];
+    b = [data(:,i); zeros(nWaves-1,1)];
+    
+    problem.H = A'*A;
+    problem.f = -A'*b;
+    problem.lb = zeros(nWaves,1);
+    problem.solver = 'quadprog';
+    problem.options = optimoptions(problem.solver,'MaxIter',1000,...
+        'TolCon',1e-12,'Display','off');
+    
+    resp = quadprog(problem);
     
     responsivity(:,i) = resp;
     
